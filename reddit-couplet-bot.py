f0b5df93a95de844e5b07bd21adb2b4c2251f4dc
YifanM <yifan.ma@hotmail.com>
2017-10-17 21:51:42 -0400

    some refactoring

--- reddit-couplet-bot.py
+++ reddit-couplet-bot.py
@@ -3,6 +3,7 @@ import time
 import string
 import re
 import requests
+from datetime import datetime
 
 #use regex instead for these strings?
 
@@ -14,6 +15,7 @@ connector_punctuation = set(";:,")
 min_syllables = 8
 max_syllables = 12
 apiBaseUrl = "https://api.datamuse.com/words?rel_rhy="
+subredditName = "yifantest"
 
 def authenticate():
     reddit = praw.Reddit("reddit-couplet-bot")
@@ -22,9 +24,13 @@ def authenticate():
 def in_range(num):
     return num >= min_syllables and num <= max_syllables
 
-    #1 syllable per vowel assuming it satisfies following conditions:
-        #don't count suffixes that end with -es -ed -e
-        #consecutive vowels only counts as one
+def remove_punctuation(word):
+    return "".join([c for c in word if c not in punctuation])
+
+#a heuristic to determine the number of syllables in a word
+#1 syllable per vowel assuming it satisfies following conditions:
+#don't count suffixes that end with -es -ed -e
+#consecutive vowels only counts as one
 def syllables(line):
     num_syllables = 0
     for word in line:
@@ -49,26 +55,19 @@ def valid_syllables(line_1, line_2):
     return syllables_1 == syllables_2
 
 def does_rhyme(word_1, word_2):
+    # after some research, rhyming appears to be much harder than counting syllables
+    # it requires converting a word to its phonetics and stresses (which seems to be very hard to do)
+    # so, we are retrieving rhyming data from an external database instead
     response = requests.get(apiBaseUrl + remove_punctuation(word_1))
-    for obj in response.json():
-        if obj["word"] == remove_punctuation(word_2):
-            return True
-    return False
-def remove_punctuation(word):
-    return "".join([c for c in word if c not in punctuation])
+    return any(obj["word"] == remove_punctuation(word_2) for obj in response.json());
 
 def validate_line(line):
-    #print (line)
-    if not line: return False
-    for word in line: #combine these 3 cases together somehow
-        if all(char.lower() not in vowels and char.lower() not in consonants for char in word):
-            #print(1)
-            return []
-        elif any(char.lower() not in vowels and char.lower() not in consonants and char not in punctuation for char in word):
-            #print(2)
-            return []
-        elif word != str(line[-1]) and any(char in end_punctuation for char in word):
-            #print(3)
+    # if not line: return False
+    for word in line:
+        if all(char.lower() not in vowels and char.lower() not in consonants for char in word) or
+           any(char.lower() not in vowels and char.lower() not in consonants and char not in punctuation for char in word) or
+           (word != str(line[-1]) and any(char in end_punctuation for char in word)):
+
             return []
     return line
 
@@ -76,27 +75,24 @@ def run_couplet_bot(subreddit):
     for comment in subreddit.comments(limit = 100):
         lines = list(map(validate_line, [line.split() for line in re.split("\n\n\n*", comment.body)]))
         for line_1, line_2 in zip(lines, lines[1:]):
-            if not line_1 or not line_2: continue
-            elif any(char in connector_punctuation for char in line_2[-1]): continue
+            if (not line_1 or not line_2) or (any(char in connector_punctuation for char in line_2[-1])):
+                continue
             if valid_syllables(line_1, line_2) and does_rhyme(line_1[-1], line_2[-1]):
-                comment_record_r = open("commented.txt", "r")
-                if comment.id not in comment_record_r.read():
-                    try:
-                        comment.reply("*" + " ".join(line_1) + "*\n\n*" + " ".join(line_2) + "*\n\n" + "Nice couplet! You're a poet! And I'm a bot.")
-                        comment_record_r.close()
-                        comment_record_w = open("commented.txt", "a+")
-                        comment_record_w.write(comment.id + "\n")
-                        comment_record_w.close()
-                    except:
-                        a = 1
-                        # do nothing for now
+                with open("commented.txt", "a+") as comment_record:
+                    if comment.id not in comment_record.read(): # check that this still works
+                        try:
+                            comment.reply(">*" + " ".join(line_1) + "*\n\n>*" + " ".join(line_2) + "*\n\n" + "Nice couplet! You're a poet! And I'm a bot.")
+                            comment_record.write(comment.id + "\n")
+                        except Exception as e:
+                            with open("exceptions.txt", "a+") as exceptions_log:
+                                exceptions_log.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + str(e) + "\n")
                 break
-    time.sleep(60)
 
 def main():
     reddit = authenticate()
-    subreddit = reddit.subreddit("yifantest")
+    subreddit = reddit.subreddit(subredditName)
     while True:
         run_couplet_bot(subreddit)
+        time.sleep(60)
 
 main()
